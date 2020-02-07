72b17ab57625447cd1b864b72ae418f3c9bd4191
Irron Williams <irron.williams@gmail.com>
2020-02-06 23:28:12 -0800

    Update Basic Data Structures.js

--- Basic Data Structures.js
+++ Basic Data Structures.js
@@ -519,4 +519,154 @@ function addFriend(userObj, friend) {
 userObj.data.friends.push(friend)
 return userObj.data.friends
 }
-console.log(addFriend(user4, 'Stone'));
+console.log(addFriend(user4, 'Stone'))
+
+/* Summary
+//to retrieve the "a" from ourArray and assign it to a variable.
+let ourArray = ["a", "b", "c"]
+let ourVariable = ourArray[0] 
+
+//Using bracket notation, can reset the item at index 1 from "b", to "not b anymore". 
+ourArray[1] = "not b anymore"
+
+the push() method adds elements to the end of an array, and unshift() adds elements to the beginning. 
+ 
+let twentyThree = 'XXIII'
+let romanNumerals = ['XXI', 'XXII']
+
+romanNumerals.unshift('XIX', 'XX')
+romanNumerals.push(twentyThree)
+
+pop() removes an element from the END of an array, while shift() removes an element from the BEGINNING. 
+pop() and shift() returns the value of the removed element:
+let greetings = ['whats up?', 'hello', 'see ya!']
+greetings.pop()
+greetings.shift()
+
+splice() allows you to remove any number of consecutive elements from ANYWHERE in an array.
+splice() also returns a new array containing the value of the removed elements
+removes 2 elements beginning with the 3rd element
+let array = ['today', 'was', 'not', 'so', 'great']
+array.splice(2, 2)
+
+splice() can take a 3rd parameter. here adding the numbers 13 and 14 to an array
+numbers.splice(startIndex, amountToDelete, 13, 14)
+
+slice() copies, or extracts, a given number of elements to a new array, leaving the array it is called upon 
+untouched. (extraction will occur up to, but NOT including the element at this index)
+
+let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear']
+let todaysWeather = weatherConditions.slice(1, 3) //slice beginning at index 1 up to but not including index 3
+
+The spread operator allows for copying all of an array's elements, in order. 
+The spread syntax simply looks like this: ...
+ 
+let thisArray = [true, true, undefined, false, null]
+let thatArray = [...thisArray]
+
+indexOf() allows you to quickly and easily check for the presence of an element on an array. indexOf() takes an 
+element as a parameter, and when called, it returns the position, or index, of that element, 
+or -1 if the element does not exist on the array.
+
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears']
+fruits.indexOf('dates') // returns -1
+fruits.indexOf('oranges') // returns 2
+fruits.indexOf('pears') // returns 1, the first index at which the element exists
+
+*Iterate Through All an Array's Items Using For Loops.  Using a for loop, this function iterates through and 
+accesses each element of the array, and subjects it to a simple test (if statement) Program easily and 
+programmatically determines which data items are greater than 10, and returned a new array containing those items.
+function greaterThanTen(arr) {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 10) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+} 
+console.log(greaterThanTen([2, 12, 8, 14, 80, 0, 1]))
+
+*myNestedArray has combination of strings, numbers, and booleans for data elements.  myNestedArray has five levels of depth.
+The outer-most array is level 1.  On the third level, the string 'deep' is included.  On the fourth level, the string 'deeper' is incl
+and on the fifth level, the string 'deepest' is included.  Logic for nested arrays as follows: 
+
+let myNestedArray = [ //[] 1 level deep (outer most array )
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'], //[ [] ] 2 levels deep 
+  ['loop', 'shift', 6, 7, 1000, 'method'], //[ [] ] 2 levels deep
+  ['concat', false, true, 'spread', 'array',['deep']], //[  [ [] ]  ] //3 levels deep 
+  ['mutate', 1327.98, 'splice', 'slice', 'push',[['deeper']]], //[ [ [  []   ]    ]   ] 4 levels deep
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth',[[['deepest']]]] // [[[[[]]]]] 5 levels deep
+ ]
+
+*Using dot notation, I can access the number of followers FCC_User has.  
+let userData = FCC_User.followers
+
+Alternatively, I can also access the property with bracket notation.  
+let userData = FCC_User['followers']
+
+
+let FCC_User = {
+  username: 'awesome_coder',
+  followers: 572,
+  points: 1741,
+  completedProjects: 15
+}
+
+*Can also add new key-value pairs to object. Adding entries for bananas, grapes and strawberries. Dot notation does not work when adding multiple entries?
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+}
+foods['bananas'] = 13
+foods['grapes'] = 35
+foods['strawberries'] = 27
+console.log(foods)
+
+*Can use the delete keyword to remove a key-value pair from an object. Using delete to remove the oranges, plums, and strawberries 
+keys from the foods3 object.
+
+let foods3 = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+}
+delete foods3.oranges
+delete foods3.plums
+delete foods3.strawberries
+
+*To check if an object has a specific property, you can use the hasOwnProperty() method and the 'in' keyword. 
+If we have an object users with a property of Alan, we could check for its presence in either of the following ways:
+
+users.hasOwnProperty('Alan');
+'Alan' in users;
+// both return true
+
+*To Iterate Through the Keys of an Object, use a for...in Statement
+
+for (let user in users) {
+  console.log(user);
+}
+
+*Generate an Array of All Object Keys with Object.keys()
+
+Can also generate an array which contains all the keys stored in an object using the Object.keys() method and passing in an object as 
+the argument. This will return an array with strings representing each property in the object. 
+There will be no specific order to the entries in the array.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
